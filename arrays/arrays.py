a7f1562eada6bb282121fc8ba6da9748e70f6251
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-12-13 14:23:14 +0100

    added array module image

---
+++ arrays/arrays.py
@@ -0,0 +1,15 @@
+import array
+
+my_array = array.array('i')
+array1 = array.array('i', [1,2,3,4,5])
+arr2 = array1.insert(8, 23)
+
+test_arr = array.array('i', [1,2,3,4,5,5])
+test_arr.append(4)
+
+test_arr2 = array.array('i', [1,2,3,4,5,5])
+test_arr2.extend({5,5,5})
+print(test_arr2)
+
+
+
