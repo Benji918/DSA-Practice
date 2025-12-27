338d85dd887574bd06f181c600eb129864e9290c
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-12-27 23:05:15 +0100

    chore: dictionary section

---
+++ hasmaps/TraverseDic.py
@@ -0,0 +1,6 @@
+def traverse_dic(dic):
+    for i, v in enumerate(dic):
+        print(i,v)
+
+test_dic = {'one': 1, 'two': 2}
+traverse_dic(test_dic)
--- hasmaps/createDictionaries.py
+++ hasmaps/createDictionaries.py
@@ -4,3 +4,7 @@ print(my_dic)
 my_dic2 = dict(one='1', two='2')
 my_dic2['3'] = 'thre'
 print(my_dic2)
+
+
+
+
