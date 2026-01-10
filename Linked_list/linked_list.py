006505c894969250e8c370927b55c53e56ea3622
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-01-10 13:45:19 +0100

    chore: learning how to add elements to a linked_list(prepend/append)

--- Linked_list/linked_list.py
+++ Linked_list/linked_list.py
@@ -18,18 +18,43 @@ class Linkdedlist:
         self.tail = None
         self.length = 0
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head # the next of the new node points to the previous head
+            self.head = new_node # update the head to be the new node
+        self.length += 1
+
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tail.next = new_node
-            self.tail = new_node
+            self.tail.next = new_node # the next of the current tail points to the new node
+            self.tail = new_node # update the tail to be the new node
         self.length += 1
+
+    def __str__(self):
+        result = []
+        node = self.head
+        while node is not None:
+            result.append(str(node.value))
+            node = node.next
+        return " -> ".join(result)
+
+
+
 
 linked_list = Linkdedlist()
 linked_list.append(20)
 linked_list.append(202)
-print(linked_list.head.value, linked_list.tail.value)
+linked_list.append(202)
+linked_list.prepend(10)
+linked_list.prepend(10)
+
 print(linked_list.length)
+print(linked_list)  # Output: 20 -> 202
