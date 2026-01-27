4d3f541830ed5946bf367b15f462dc5ed8cfc9ee
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-01-27 10:11:53 +0100

    chore: implement circular singly linked list with append and display methods

---
+++ Linked_list/circular_linked_list/CSLinked_list.py
@@ -0,0 +1,48 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class CSLinkedList():
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        new_node.next = self.head
+        self.tail = new_node
+        self.length = 1
+
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            new_node.next = self.head
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.tail = new_node
+        self.length += 1
+
+    def display(self):
+        elements = []
+        if not self.head:
+            return elements
+
+        current = self.head
+        while True:
+            elements.append(current.value)
+            current = current.next
+            if current == self.head:
+                break
+        return elements
+
+
+cslinkedlist = CSLinkedList(1)
+cslinkedlist.append(2)
+cslinkedlist.append(3)
+print(cslinkedlist.head.next.value)  # Output: 2
+
+print(cslinkedlist.display())  # Output: [1, 2, 3]
