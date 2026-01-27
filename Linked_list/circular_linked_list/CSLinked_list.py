2b3d64626b1600ab89e519366b62e1b8b3065e03
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-01-27 10:44:26 +0100

    feat: add prepend and insert methods to circular singly linked list

--- Linked_list/circular_linked_list/CSLinked_list.py
+++ Linked_list/circular_linked_list/CSLinked_list.py
@@ -35,15 +35,60 @@ class CSLinkedList():
         while current.next != self.head:
             elements.append(str(current.value))
             current = current.next
+
             if current == self.head:
                 break
         return ' -> '.join(elements)
+
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0 or self.head is None and self.tail is None:
+            self.head = new_node
+            new_node.next = self.head
+            self.tail = new_node
+
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.tail.next = self.head
+        self.length += 1
+
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            raise IndexError('Index out of bounds!')
+
+        if index == 0:
+            self.prepend(value)
+            return
+
+        if index == self.length:
+            self.append(value)
+            return
+
+        new_node = Node(value)
+        current = self.head
+        prev = None
+
+
+        for _ in range(index -1):
+            prev = current
+            current = current.next
+
+        prev.next = new_node
+        new_node.next = current
+
+        self.length += 1
 
 
 cslinkedlist = CSLinkedList(1)
 cslinkedlist.append(2)
 cslinkedlist.append(3)
 cslinkedlist.append(3)
-print(cslinkedlist.head.next.value)  # Output: 2
+cslinkedlist.prepend(45)
+cslinkedlist.prepend(45)
+cslinkedlist.insert(3,40)
 
-print(cslinkedlist.display())  # Output: [1, 2, 3]
+print(cslinkedlist.display())
