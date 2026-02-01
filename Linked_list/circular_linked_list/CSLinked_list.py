d26622a4514d88c84122744e80019f3a1fb88073
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-02-01 20:54:14 +0100

    feat: performed the remove on a CS linkedlist

--- Linked_list/circular_linked_list/CSLinked_list.py
+++ Linked_list/circular_linked_list/CSLinked_list.py
@@ -72,6 +72,18 @@ class CSLinkedList():
             index += 1
         return False
 
+    def get(self, index):
+        if index >= self.length:
+            raise IndexError('Index out of bounds!')
+
+        if index < 0:
+            index = self.length + index
+
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+
 
     def insert(self, index, value):
         if index < 0 or index > self.length:
@@ -100,13 +112,89 @@ class CSLinkedList():
         self.length += 1
 
 
+    def set_value(self, index, value):
+        if index == 0:
+            return self.prepend(value)
+
+        if index == self.length:
+            return self.append(value)
+
+        if index >= self.length:
+            raise IndexError('Index out of bounds!')
+
+        if index < 0:
+            index = self.length + index
+
+        get_value = self.get(index)
+        if get_value:
+            get_value.value = value
+            return True
+        return False
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+
+        temp = self.head
+        self.head = self.head.next
+        self.length -= 1
+        self.tail.next = self.head
+        temp.next = None
+
+        return temp.value
+
+    def pop(self):
+        if self.length == 0:
+            return self.pop_first()
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+
+        temp = self.tail
+        value = self.get(self.length - 2)
+        self.tail = value
+        self.tail.next = self.head
+        self.length -= 1
+
+        return temp.value
+
+    def remove(self, index):
+        if self.length == 0:
+            return self.pop_first()
+
+        if index == self.length:
+            return self.pop()
+
+        poppped_node = self.get(index)
+        prev_node = self.get(index - 1)
+
+        prev_node.next = poppped_node.next
+        poppped_node.next = None
+
+        self.length -=1
+
+
+
+
+
+
 cslinkedlist = CSLinkedList(1)
 cslinkedlist.append(2)
 cslinkedlist.append(3)
 cslinkedlist.append(3)
-cslinkedlist.prepend(45)
+cslinkedlist.prepend(459)
 cslinkedlist.prepend(45)
 cslinkedlist.insert(3,40)
-print(cslinkedlist.search(40))
+# print(cslinkedlist.search(40))
+# print(cslinkedlist.get(3))
+# print(cslinkedlist.set_value(index=3, value=100))
+print(cslinkedlist.display())
+print(cslinkedlist.remove(2))
+# print(cslinkedlist.pop())
 
 print(cslinkedlist.display())
